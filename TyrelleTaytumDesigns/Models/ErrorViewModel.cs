3ae6572530825da70a365b6b9a2991fa9c1b9922
KeenenJ <106118912+KeenenJ@users.noreply.github.com>
2026-08-07 10:18:23 +0200

    Initial commit

---
+++ TyrelleTaytumDesigns/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TyrelleTaytumDesigns.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
