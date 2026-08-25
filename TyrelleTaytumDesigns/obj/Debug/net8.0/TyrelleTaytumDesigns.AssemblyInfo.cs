b7fb9753143dc0e63dca3d7299f028dd616e0f76
KeenenJ <106118912+KeenenJ@users.noreply.github.com>
2026-08-25 15:30:32 +0200

    Merge website updates and resolve conflicts

--- TyrelleTaytumDesigns/obj/Debug/net8.0/TyrelleTaytumDesigns.AssemblyInfo.cs
+++ TyrelleTaytumDesigns/obj/Debug/net8.0/TyrelleTaytumDesigns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TyrelleTaytumDesigns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9954775beb47a3b8a1406c6503cdf9dbb730798a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TyrelleTaytumDesigns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TyrelleTaytumDesigns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
